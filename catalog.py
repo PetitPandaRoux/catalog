adea9fd276c590b7a1c6730ae2ff94dafe8cd554
Sakada Ly <lysakada@gmail.com>
2018-12-01 18:22:10 +0100

    main page done

--- catalog.py
+++ catalog.py
@@ -1,24 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-#from setup_db import Machine, User
+from setup_database import Base, Member, Machine, Project, Tag, Book
 
 app = Flask(__name__)
 
 #DataBase
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
-#
 DBSession = sessionmaker(bind=engine)
+
 
 
 @app.route('/')
 @app.route('/catalog/')
 def show_catalog():
-    return "Voici le catalog"
+    return render_template('catalog.html')
 
 @app.route('/catalog/login')
 def show_connection():
@@ -28,17 +28,16 @@ def show_connection():
 def show_machines():
     return "Voici les machines"
 
-@app.route('catalog/tools')
+@app.route('/catalog/tools')
 def show_outils():
     return "Voici les outils du lpfp"
 
-@app.route('catalog/books')
+@app.route('/catalog/books')
 def show_books():
-    return "Voici les livres présent au sein du lpfp"
+    return "Voici les livres present au sein du lpfp"
 
-@app.route('')
 
-@app.route('catalog/members')
+@app.route('/catalog/members')
 def show_members():
     return "Voici les membres du lpfp"
 
