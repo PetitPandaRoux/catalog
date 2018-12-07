e987a9e2459f264ae018738bf69aa2a8f73a846b
Sakada Ly <lysakada@gmail.com>
2018-12-07 09:13:38 +0100

    correcting setup_database style to PEP8

--- setup_database.py
+++ setup_database.py
@@ -8,28 +8,31 @@ from flask_dance.consumer.backend.sqla import OAuthConsumerMixin
 
 Base = declarative_base()
 
+
 class Member(Base):
     __tablename__ = 'member'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    inscription_date = Column(String(250)) #DD/MM/YY
+    inscription_date = Column(String(250))  # DD/MM/YY
     picture = Column(String(250), default="blank_user.gif")
 
     @property
     def serialize(self):
-        #Returns object data in easily serializable format
+        # Returns object data in easily serializable format
         return{
-            'name' : self.name,
-            'email' : self.email, 
-            'id' : self.id,
-            'inscription_date':self.inscription_date,    
+            'name': self.name,
+            'email': self.email,
+            'id': self.id,
+            'inscription_date': self.inscription_date,
         }
+
 
 class Oauth(OAuthConsumerMixin):
     user_id = Column(Integer, ForeignKey(Member.id))
     user = relationship(Member)
+
 
 class Machine(Base):
     __tablename__ = 'machine'
@@ -40,19 +43,20 @@ class Machine(Base):
     company = Column(String(250))
     description = Column(String(250))
     picture = Column(String(250), default="blank_machine.gif")
-    price = Column(Integer) # euros
+    price = Column(Integer)  # euros
 
     @property
     def serialize(self):
-        #Returns object data in easily serializable format
+        # Returns object data in easily serializable format
         return {
-            'name' : self.name,
-            'machine_type' : self.machine_type, 
-            'id' : self.id,
-            'description':self.description,
-            'price':self.price,
-            'company':self.company,
+            'name': self.name,
+            'machine_type': self.machine_type,
+            'id': self.id,
+            'description': self.description,
+            'price': self.price,
+            'company': self.company,
         }
+
 
 class Project(Base):
     __tablename__ = 'project'
@@ -62,7 +66,7 @@ class Project(Base):
     description = Column(String(450))
     picture = Column(String(250), default="blank_project.gif")
     source = Column(String(850))
-    end_date = Column(String(10)) #DD/MM/YY
+    end_date = Column(String(10))  # DD/MM/YY
     member_id = Column(Integer, ForeignKey('member.id'))
     member = relationship(Member)
 
@@ -70,20 +74,24 @@ class Project(Base):
     def serialize(self):
         return {
             'name': self.name,
-            'id':self.id,
-            'description':self.description,
-            'source':self.source,
-            'end_date':self.end_date,
+            'id': self.id,
+            'description': self.description,
+            'source': self.source,
+            'end_date': self.end_date,
             'member_id': self.member_id,
         }
+
 
 class Tag(Base):
     __tablename__ = 'tag'
 
     id = Column(Integer, primary_key=True)
-    tag_name = Column(String(100), nullable=False) # we use 5 tags : Arduino, 3D Printer, Laser Cutter, Portable Electric, No Tags
+    # 5 tag_name : Arduino, 3D Printer, Laser Cutter,
+    # Portable Electric, No Tags
+    tag_name = Column(String(100), nullable=False)
     project_id = Column(Integer, ForeignKey('project.id'))
     project = relationship(Project)
+
 
 engine = create_engine('sqlite:///lepetitfablabdeparis.db', encoding='utf-8')
 Base.metadata.create_all(engine)
