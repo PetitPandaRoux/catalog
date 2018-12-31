3dd17533009bed0025a368b8c48b09f60c95e5b7
Sakada Ly <lysakada@gmail.com>
2018-12-31 13:21:07 +0100

    changed port for venv

--- catalog.py
+++ catalog.py
@@ -363,4 +363,4 @@ def show_projects_tag(tag_name):
 if __name__ == '__main__':
     app.secret_key = "super_secret_key"
     app.debug = True
-    app.run(host='0.0.0.0', port=5000, threaded=False)
+    app.run(port=8800, threaded=False)
